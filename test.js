43265c0da21d97fc5f70b987967a75a734f8ca96
Richard Astbury <richard.astbury@gmail.com>
2019-01-17 09:51:34 +1100

    add md5 hash

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const files = require('./lib/files')
+const md5 = require('./lib/md5')
 
 describe('lib/files', () => {
   it('lists all files recursively', done => {
@@ -6,6 +7,18 @@ describe('lib/files', () => {
 
     if (fileList.indexOf('lib/files.js') === -1)
       return done(`filed to find expected file [${fileList.join()}]`)
+
+    done()
+  })
+})
+
+describe('lib/md5', () => {
+  it('calculates the md5 hash of a file', done => {
+    var hash = md5('./lib/md5.js')
+
+    if (hash.toLowerCase() !== '96C0D686B60669D9EB74FD9C68123E02'.toLowerCase()) {
+      return done(`unexpected hash value (${hash})`)
+    }
 
     done()
   })
