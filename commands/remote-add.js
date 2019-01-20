f397e6550a713bb132b79203271c39bc15da71b0
Richard Astbury <richard.astbury@gmail.com>
2019-01-20 11:31:37 +1100

    added storage, and add remote

--- commands/remote-add.js
+++ commands/remote-add.js
@@ -1,3 +1,29 @@
-module.exports = (argv) => {
-  console.log("add remote")
+const commandLineArgs = require('command-line-args')
+const storage = require('../lib/storage')
+const REMOTES = 'REMOTES'
+
+const definitions = [
+  { name: 'name', defaultOption: true },
+  { name: 'connection' }
+]
+
+module.exports = argv => {
+  const options = commandLineArgs(definitions, {
+    argv,
+    stopAtFirstUnknown: true
+  })
+
+  let [name, connection] = options._unknown
+
+  addRemote(name, connection)
+    .then(() => console.log('Remote added'))
+    .catch(err => console.log('error', err))
+}
+
+async function addRemote(name, connection) {
+  const remotes = (await storage.get(REMOTES)) || {}
+  remotes[name] = {
+    connection
+  }
+  await storage.put(REMOTES, remotes)
 }
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const files = require('./lib/files')
 const md5 = require('./lib/md5')
 const discoverLocalFiles = require('./lib/discoverLocalFiles')
+const storage = require('./lib/storage')
 
 describe('lib/files', () => {
   it('lists all files recursively', done => {
@@ -17,7 +18,9 @@ describe('lib/md5', () => {
   it('calculates the md5 hash of a file', done => {
     const hash = md5('./lib/md5.js')
 
-    if (hash.toLowerCase() !== '96C0D686B60669D9EB74FD9C68123E02'.toLowerCase()) {
+    if (
+      hash.toLowerCase() !== '96C0D686B60669D9EB74FD9C68123E02'.toLowerCase()
+    ) {
       return done(`unexpected hash value (${hash})`)
     }
 
@@ -26,11 +29,30 @@ describe('lib/md5', () => {
 })
 
 describe('lib/discoverLocalFiles', () => {
-  it ('return the path and hash of all files in a directory', done => {
+  it('return the path and hash of all files in a directory', done => {
     const fileList = discoverLocalFiles('./lib')
     if (fileList.length === 0) return done('no files returned')
     if (!fileList[0].path) return done('no path')
     if (!fileList[0].md5) return done('no md5')
     return done()
+  })
+})
+
+describe('lib/storage', () => {
+  it('saves and retrieves values', async () => {
+    await storage.put('test', { foo: 'bar' })
+    const val = await storage.get('test')
+    if (!val) throw new Error('no value')
+
+    if (val.foo !== 'bar')
+      throw new Error(`unexepected value in the baggage area (${val})`)
+  })
+})
+
+describe('lib/storage', () => {
+  it('getting a null', async () => {
+    const val = await storage.get('XXX')
+    if (val) throw new Error('no value')
+
   })
 })
